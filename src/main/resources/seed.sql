84921f85745b8ce79e87b8358cd3ffcaf581fc38
realguites2 <guilherme.tes@gmail.com>
2024-09-22 20:59:49 +0000

    Adicionado seed para empresa default

--- src/main/resources/seed.sql
+++ src/main/resources/seed.sql
@@ -1,1 +1,2 @@
 INSERT INTO USUARIO(email, nivel, nome, senha, id_empresa) values ('guilherme.tes@gmail.com', 0, 'Guilherme Tessmann', '$2a$10$GiseHkdvwOFr7A9KRWbeiOmg/PYPhWVjdm42puLfOzR/gIAQrsAGy',1)
+INSERT INTO EMPRESA(cnpj, ddd, nome_contato, nome_fantasia, razao_social, telefone, tipo_juridico) values ('95736374000186','53', 'Guilherme Tessmann', 'TimeTask', 'TimeTask','997121788', 0);
